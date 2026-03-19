54985c461153ecb62d0af91f0149d0215ea6c4ae
sunny <282505458@qq.com>
2026-03-19 11:48:59 +0800

    fix: 更新next配置满足cloudflare部署

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // 全局启用 Edge Runtime
+  experimental: {
+    runtime: 'edge', // 所有路由默认使用 Edge 运行时
+  },
   images: {
     formats: ["image/avif", "image/webp"]
   }
